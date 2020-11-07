e1e6363a6e9248481b0facbe75914ab96c3bc379
Suslikov Danill <danyvs@mail.ru>
2020-11-07 13:13:06 +0300

    save

--- AA3/tests.cpp
+++ AA3/tests.cpp
@@ -5,29 +5,49 @@ void test()
     int* array = nullptr;
     int* copy_arr = nullptr;
 
-    for (int len = 0; len <= 500; len+= 100)
+    for (int len = 100; len <= 500; len+= 100)
     {
+        std::cout << "LEN " << len << std::endl;
         copy_arr = create_array(len);
         array = create_array(len);
         fill_array_with_random_nums(array, len);
+        //reverse_bubble_sort(array, len);
 
+        float sum = 0;
         copy_array(array, len, copy_arr);
-        clock_t start = clock();
-        bubble_sort(copy_arr, len);
-        clock_t end = clock();
-        std::cout << "Bubble sort time: " << (float)(end - start) << std::endl;
+        for (int _ = 0; _ < 10; _++)
+        {
+            copy_array(array, len, copy_arr);
+            clock_t start = clock();
+            bubble_sort(copy_arr, len);
+            clock_t end = clock();
+            sum += (float)(end - start);
+        }
+        std::cout << "Bubble sort time: " << sum / 10 << std::endl;
 
+        sum = 0;
         copy_array(array, len, copy_arr);
-        start = clock();
-        insertion_sort(copy_arr, len);
-        end = clock();
-        std::cout << "Insertion sort time: " << (float)(end - start) << std::endl;
+        for (int _ = 0; _ < 10; _++)
+        {
+            copy_array(array, len, copy_arr);
+            clock_t start = clock();
+            insertion_sort(copy_arr, len);
+            clock_t end = clock();
+            sum += (float)(end - start);
+        }
+        std::cout << "Insertion sort time: " << sum / 10 << std::endl;
 
+        sum = 0;
         copy_array(array, len, copy_arr);
-        start = clock();
-        choices_sort(copy_arr, len);
-        end = clock();
-        std::cout << "Choices sort time: " << (float)(end - start) << std::endl << std::endl;
+        for (int _ = 0; _ < 10; _++)
+        {
+            copy_array(array, len, copy_arr);
+            clock_t start = clock();
+            choices_sort(copy_arr, len);
+            clock_t end = clock();
+            sum += (float)(end - start);
+        }
+        std::cout << "Choices sort time: " << sum / 10 << std::endl << std::endl;
 
         delete_array(array);
         delete_array(copy_arr);
